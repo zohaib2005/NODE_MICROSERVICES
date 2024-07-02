99faece51d0ab06303c855a3b47b07d03e398266
zohaib <zohaibshafiq2005@gmail.com>
2024-07-02 14:50:02 +0500

    put and delete

--- workspace/microservices/catalog-service/routes/index.js
+++ workspace/microservices/catalog-service/routes/index.js
@@ -17,6 +17,9 @@ router.get("/items", async (req, res) => {
 router.get("/items/:id", async (req, res) => {
   try {
     const item = await CatalogService.getOne(req.params.id);
+    if (!updatedItem) {
+      return res.status(404).json({ error: "Item not found." });
+    }
     return res.json(item);
   } catch (error) {
     console.error(error);
@@ -28,6 +31,32 @@ router.post("/items", async (req, res) => {
   try {
     const newItem = await CatalogService.create(req.body);
     return res.json(newItem);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.put("/items/:id", async (req, res) => {
+  try {
+    const updatedItem = await CatalogService.update(req.params.id, req.body);
+    if (!updatedItem) {
+      return res.status(404).json({ error: "Item not found." });
+    }
+    return res.json(updatedItem);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.delete("/items/:id", async (req, res) => {
+  try {
+    const deletionResult = await CatalogService.remove(req.params.id);
+    if (!deletionResult.deleteCount === 0) {
+      return res.status(404).json({ error: "Item not found." });
+    }
+    return res.status(204).send();
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Internal server error" });
